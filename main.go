6a756653de265bfa98bcc41bfc87f5a6ce0f9a72
Nitin Sharma <snitin@uber.com>
2024-09-05 15:40:56 -0700

    Watch

--- main.go
+++ main.go
@@ -161,3 +161,4 @@ func ListPods(namespace string, ncs NameClientset) (*v1.PodList, error) {
 }
 
 //https://github.com/karmada-io/karmada/tree/master/operator
+// https://www.cncf.io/blog/2022/09/26/karmada-and-open-cluster-management-two-new-approaches-to-the-multicluster-fleet-management-challenge/
